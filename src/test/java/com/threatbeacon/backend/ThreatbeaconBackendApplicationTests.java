d3f07f7e46b22657c528a1be5569ada8cde0d2ca
Andres gonzalez <andresgonzalez25022@gmail.com>
2025-11-29 15:05:00 -0500

    initialize project structure with Maven and Spring Boot setup

---
+++ src/test/java/com/threatbeacon/backend/ThreatbeaconBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threatbeacon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreatbeaconBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
