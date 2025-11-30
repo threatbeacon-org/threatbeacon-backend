9c8defda6eee762ee1de3a30c96982ca33673927
Andres Garcia. <55115368+alexx4316@users.noreply.github.com>
2025-11-29 23:38:00 -0500

    Revert "Feature: Event Entity and Repository (T2.2.1 & T2.2.2)"

--- src/main/java/com/threatbeacon/backend/beacon/BeaconStateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.threatbeacon.backend.beacon;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Repository for accessing and managing the single BeaconState entity.
- */
-public interface BeaconStateRepository extends JpaRepository<BeaconState, Long> {
-}
